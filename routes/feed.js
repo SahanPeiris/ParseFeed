431388bb6fc2b19a324678cb28e6040f20aed99a
Fan Xia <frankxia0404@gmail.com>
2015-07-22 18:31:56 -0700

    Refactored Feed Using Promise

--- routes/feed.js
+++ routes/feed.js
@@ -1,178 +1,185 @@
 /**
- * Created by fanxia on 7/20/15.
+ * Created by fanxia on 7/22/15.
  */
 var express = require('express');
-var request = require('request');
+var rp = require('request-promise');
 var RateLimiter = require('limiter').RateLimiter;
 var Q = require('q');
+var _ = require('underscore');
 var router = express.Router();
 
-/* GET feed */
-router.get('/:feedNum', function (req, res, next) {
-    var feedNum = req.params.feedNum;
-    var offset = 0;
-    console.log(feedNum);
-    var apiKey = '9fvuooi2ro6lugj1f554tw68';
-    loadEtsyFeed(apiKey, feedNum, offset, function (err, data) {
-        if (err) {
-            console.log(err.message);
-        }
-        else {
-            res.send({count: data.length, results: data})
-        }
-    });
-
+/**
+ * Support n api_key and offset
+ */
+router.get('/', function (req, res, next) {
+    var defaultFeedNum = 10;
+    res.redirect(req.baseUrl + '/' + defaultFeedNum);
 });
 
-function loadEtsyFeed(apiKey, limit, offset, cb) {
-    var limiter = new RateLimiter(1, 150);
-    var listings = [];
-    Q.when(null)
-        .then(function () {
-            var deferred = Q.defer();
+router.get('/:feedNum', function (req, res, next) {
+    var feedNum = req.params.feedNum;
+    var offset = req.query.offset || 0;
+    console.log(feedNum, offset);
 
-            var reqConfig = {
-                url: 'https://openapi.etsy.com/v2/listings/active',
-                qs: {
-                    api_key: apiKey,
-                    limit: limit,
-                    offset: offset
-                },
-                method: 'GET'
-            };
-            rateLimitRequest(reqConfig, limiter, function (err, resp, body) {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    var jsonBody = JSON.parse(body);
-                    var totalListingCount = jsonBody.count;
-                    if (totalListingCount > 0) {
-
-                        deferred.resolve(jsonBody.results);
-                    }
-                    else {
-                        deferred.reject(new Error('No listings available'));
-                    }
-                }
-            });
-            return deferred.promise;
-        })
-        .then(function (results) {
-
-            var deferred = Q.defer();
-            for (var i = 0; i < results.length; i++) {
-                Q.when(results[i])
-                    .then(function (listing) {
-                        return {
-                            listing_id: listing.listing_id,
-                            title: listing.title,
-                            description: listing.description,
-                            price: listing.price,
-                            currency_code: listing.currency_code
-                        };
-                    })
-                    .then(function (listing) {
-                        var reqConfig = {
-                            url: 'https://openapi.etsy.com/v2/listings/' +
-                            listing.listing_id + '/images',
-                            qs: {
-                                api_key: apiKey
-                            },
-                            method: 'GET'
-                        };
-
-                        var deferred = Q.defer();
-                        rateLimitRequest(reqConfig, limiter,
-                            function (err, resp, body) {
-                                if (err) {
-                                    console.log(err);
-                                }
-                                else if (body.indexOf('You have') === 0) {
-                                    deferred.reject(new Error(body));
-                                }
-                                else {
-                                    var jsonBody = JSON.parse(body);
-                                    var imageCount = jsonBody.count;
-                                    if (imageCount > 0) {
-                                        var imageResult = jsonBody.results[0];;
-
-                                        var images = [imageResult.url_75x75,
-                                            imageResult.url_170x135,
-                                            imageResult.url_570xN,
-                                            imageResult.url_fullxfull];
-                                        deferred.resolve({
-                                            id: listing.listing_id,
-                                            title: listing.title,
-                                            images: images,
-                                            description: listing.description,
-                                            price: listing.price,
-                                            currency_code: listing.currency_code
-                                        });
-                                    }
-                                    else {
-                                        deferred.reject(new Error('No images' +
-                                            ' available'));
-                                    }
-                                }
-                            });
-                        return deferred.promise;
-                    })
-                    .then(function (listing) {
-                        listings.push(listing);
-                    })
-                    .then(function () {
-                        if (listings.length == results.length) {
-                            deferred.resolve(listings);
-                        }
-                    })
-                    .catch(function (err) {
-                        console.log(err.message);
-                        deferred.reject(err);
-                    })
-                    .done();
-            }
-            return deferred.promise;
-        })
-        .then(function (listings) {
-            cb(null, listings);
-        })
-        .catch(function (err) {
-            console.log(err.message);
-            cb(err);
+    var apiKeys = ['g7gl9ouuxlq1j35aqas99ai4'];
+    var workerNum = calculateWorkerNum(apiKeys.length, feedNum);
+    getEtsyFeedUsingMultipleWorkers(apiKeys, workerNum, feedNum, offset)
+        .then(function (val) {
+            res.header('Content-Type', 'application/json');
+            res.send({count: val.length, results: val});
         })
         .done();
+});
+
+function calculateWorkerNum(apiKeyNum, feedNum) {
+    if (feedNum <= 100) {
+        return apiKeyNum;
+    }
+    else {
+        return Math.ceil(feedNum / 100);
+    }
 }
 
-function rateLimitRequest(config, limiter, cb) {
-    limiter.removeTokens(1, function () {
-        request(config, cb);
+function getEtsyFeedUsingMultipleWorkers(apiKeys, workerNum, feedNum, offset) {
+    var feedPromises = [];
+
+    var apiKeyPromise = getApiKeyAsync(apiKeys);
+    var feedNumPerWorker = Math.ceil(feedNum / workerNum);
+    var feedNumLastWorker = feedNum - (workerNum - 1) * feedNumPerWorker;
+    for (var i = 0; i < workerNum - 1; i++) {
+        var feedPromise = apiKeyPromise.then(function (apiKey) {
+            return getEtsyFeedAsync(apiKey, feedNumPerWorker, offset)
+                .then(function (res) {
+                    apiKeys.push(apiKey);
+                    return res;
+                });
+        });
+        offset += feedNumPerWorker;
+        feedPromises.push(feedPromise);
+    }
+    // For the last worker
+    var lastFeedPromise = apiKeyPromise.then(function (apiKey) {
+        return getEtsyFeedAsync(apiKey, feedNumLastWorker, offset)
+            .then(function (res) {
+                apiKeys.push(apiKey);
+                return res;
+            });
+    });
+    feedPromises.push(lastFeedPromise);
+    return Q.all(feedPromises).then(function (res) {
+        var shallowFaltten = true;
+        return _.flatten(res, shallowFaltten);
     });
 }
 
-/** Get from Stackoverflow post
- *  (http://stackoverflow.com/questions/17217736/while-loop-with-promises)
- */
-function promiseWhile(condition, body) {
-    var done = Q.defer();
+function getEtsyFeedAsync(apiKey, feedNum, offset) {
+    var limiter = new RateLimiter(1, 150);
+    var listingsPromise = getEtsyListingsAsync(limiter, apiKey, feedNum,
+        offset);
+    return listingsPromise.then(function (res) {
+        var itemPromises = [];
+        res.forEach(function (listing) {
+            itemPromises.push(getItemAsync(limiter, listing, apiKey));
+        });
+        return Q.all(itemPromises);
+    })
+}
+
+function getItemAsync(limiter, listing, apiKey) {
+    var config = {
+        url: 'https://openapi.etsy.com/v2/listings/' +
+        listing.listing_id + '/images',
+        qs: {
+            api_key: apiKey
+        },
+        method: 'GET'
+    };
+    var imagePromise = sendRequestAsync(config, limiter);
+    return imagePromise.then(function (res) {
+        var jsonBody = JSON.parse(res);
+        var imageNum = jsonBody.count;
+        if (imageNum > 0) {
+            var imageResult = jsonBody.results[0];
+
+            var images = [imageResult.url_75x75,
+                imageResult.url_170x135,
+                imageResult.url_570xN,
+                imageResult.url_fullxfull];
+
+            return {
+                id: listing.listing_id,
+                title: listing.title,
+                images: images,
+                description: listing.description,
+                price: listing.price,
+                currency_code: listing.currency_code
+            };
+        }
+        else {
+            throw new Error('No images available');
+        }
+    });
+}
+
+function getEtsyListingsAsync(limiter, apiKey, feedNum, offset) {
+    var config = {
+        url: 'https://openapi.etsy.com/v2/listings/active',
+        qs: {
+            api_key: apiKey,
+            limit: feedNum,
+            offset: offset
+        },
+        method: 'GET'
+    };
+    return sendRequestAsync(config, limiter)
+        .then(function (res) {
+            var jsonBody = JSON.parse(res);
+            var listingsNum = jsonBody.count;
+            if (listingsNum > 0) {
+                return _.map(jsonBody.results, function (listing) {
+                    return {
+                        listing_id: listing.listing_id,
+                        title: listing.title,
+                        description: listing.description,
+                        price: listing.price,
+                        currency_code: listing.currency_code
+                    };
+                });
+            }
+            else {
+                throw new Error('No listings available');
+            }
+        });
+}
+
+function sendRequestAsync(config, limiter) {
+    var def = Q.defer();
+    limiter.removeTokens(1, function (err) {
+        if (err) {
+            def.reject(err)
+        }
+        else {
+            def.resolve(rp(config));
+        }
+    });
+
+    return def.promise;
+}
+
+function getApiKeyAsync(apiKeys) {
+    var gotKey = Q.defer();
 
     function loop() {
-        // When the result of calling `condition` is no longer true, we are
-        // done.
-        if (!condition()) return done.resolve();
-        // Use `when`, in case `body` does not return a promise.
-        // When it completes loop again otherwise, if it fails, reject the
-        // done promise
-        Q.when(body(), loop, done.reject);
+        if (apiKeys.length > 0) {
+            return gotKey.resolve(apiKeys.shift());
+        }
+
+        Q.when(null, loop, gotKey.reject)
     }
 
-    // Start running the loop in the next tick so that this function is
-    // completely async. It would be unexpected if `body` was called
-    // synchronously the first time.
     Q.nextTick(loop);
 
-    // The promise
-    return done.promise;
+    return gotKey.promise;
 }
 
 module.exports = router;
